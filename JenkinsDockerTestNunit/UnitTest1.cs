8ee61d31d2224ccc8a4c3a491215c7a23dc90e4a
ahkrisztian <lunarcsik.krisztian@gmail.com>
2023-05-14 19:28:38 +0200

    remote

--- JenkinsDockerTestNunit/UnitTest1.cs
+++ JenkinsDockerTestNunit/UnitTest1.cs
@@ -6,16 +6,16 @@ namespace JenkinsDockerTestNunit
 {
     public class Tests
     {
-        IWebDriver driver { get; set; }
+        RemoteWebDriver driver;
 
         [SetUp]
         public void Setup()
         {
             var chromeOptions = new ChromeOptions();
-            chromeOptions.PageLoadStrategy = PageLoadStrategy.Normal;
-            //driver = new RemoteWebDriver(new Uri("http://localhost:4445/wd/hub"), chromeOptions);
-            driver = new ChromeDriver(chromeOptions);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), chromeOptions);
+            //driver = new ChromeDriver(chromeOptions);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+
         }
 
         [TearDown]
@@ -28,26 +28,18 @@ namespace JenkinsDockerTestNunit
         public void Test1()
         {
             driver.Navigate().GoToUrl("https://index.hu/");
-            IWebElement barat = driver.FindElement(By.XPath("//img[contains(@alt, 'Barátság')]"));
-
-            string indexvas = barat.GetAttribute("src");
-
-            Assert.IsTrue(indexvas.Contains("indexvas"));
+            
         }
 
         [Test] public void Test2()
         {
-            bool x = true;
-
-            Assert.IsTrue(x);
+            driver.Navigate().GoToUrl("https://www.google.com/");
         }
 
         [Test]
         public void Test3()
         {
-            bool x = true;
-
-            Assert.IsTrue(x);
+            driver.Navigate().GoToUrl("https://wccftech.com/");
         }
     }
 }
